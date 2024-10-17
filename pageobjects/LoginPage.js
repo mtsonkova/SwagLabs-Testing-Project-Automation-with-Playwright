fa268ed7a0a91d62f99c99903ef73376d9cbd2da
mtsonkova <mtsonkova950@gmail.com>
2024-10-17 23:06:11 +0300

    Login page functionality test cases are automated

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -8,19 +8,16 @@ class LoginPage{
         this.userName = page.locator('#user-name');
         this.password = page.locator('#password');
         this.loginBtn = page.locator('#login-button');
-        this.error = page.locator('h3');
+       
     }
 
     async login(username, pass) {
         await this.userName.fill(username);
         await this.password.fill(pass);
         await this.loginBtn.click();
+    
     }
-
-    async getErrMsg() {
-        let errMsg = await this.error.textContent();
-        return errMsg;
-    }
+    
 }
 
 module.exports = {LoginPage};
--- tests/e2e.test.js
+++ tests/e2e.test.js
@@ -2,7 +2,7 @@ const { test, describe, beforeEach, afterEach, beforeAll, afterAll, expect } = r
 const { chromium } = require('playwright');
 const testData = require('../testdata.json');
 
-const {LoginPage} = require('../pageobjects/LoginPage');
+const { LoginPage } = require('../pageobjects/LoginPage');
 
 let browser;
 let context;
@@ -10,10 +10,10 @@ let page;
 
 let loginPage;
 
-describe('e2e test cases', async() => {
+describe('e2e test cases', async () => {
     //launch chromium browser
     beforeAll(async () => {
-        browser = await chromium.launch();        
+        browser = await chromium.launch();
     });
 
     //close chromium browser
@@ -23,8 +23,8 @@ describe('e2e test cases', async() => {
 
     //create new browser context
 
-    beforeEach(async  () => {
-        context =  await browser.newContext();
+    beforeEach(async () => {
+        context = await browser.newContext();
         page = await context.newPage();
         await page.goto('/');
 
@@ -36,21 +36,25 @@ describe('e2e test cases', async() => {
         await context.close();
     });
 
-    describe('Test login and authentication functionality', async() => {
-        test('Login with valid credentials', async() => {
-           let {username, password, url} = testData[0];
-           await loginPage.login(username, password);
-           await expect(page.url()).toEqual(url);           
-        });
-        
-        test('Login with locked-out user', async() => {
-            let {username, password, errorMsg} = testData[1];
+    describe('Test login and authentication functionality', async () => {
+        test('Login with valid credentials', async () => {
+            let { username, password, url } = testData[0];
             await loginPage.login(username, password);
-            
-        })
+            await expect(page.url()).toEqual(url);
+        });
+
+        test('Login with locked-out user', async () => {
+            let { username, password, errorMsg } = testData[1];
+            await loginPage.login(username, password);
+            await expect(page.locator('h3')).toHaveText(errorMsg);
+        });
+
+        test('Login with unregistered user', async () => {
+            let { username, password, errorMsg } = testData[2];
+            await loginPage.login(username, password);
+            await expect(page.locator('h3')).toHaveText(errorMsg);
+        });
     })
-
-
 
 }
 )
