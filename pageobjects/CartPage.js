7a74998baa49d961a4b075cf841878f837465738
mtsonkova <mtsonkova950@gmail.com>
2024-10-20 00:35:15 +0300

    In progress

---
+++ pageobjects/CartPage.js
@@ -0,0 +1,22 @@
+class CartPage{
+    constructor(page) {
+        this.page = page;
+        this.cartProducts = page.locator('.cart_item');
+        this.continueShopping = page.getByRole('button', {name:'Continue Shopping'});
+        this.checkout = page.getByRole('button', {name:'Checkout'});
+    }
+
+    async clickOnContinueShopping() {
+        await this.continueShopping.click();
+    } 
+
+    async clickOnCheckout() {
+        await this.checkout.click();
+    }
+
+    async getFirstProductFromCart() {
+        return await this.cartProducts.nth(0);
+    }
+
+
+}
