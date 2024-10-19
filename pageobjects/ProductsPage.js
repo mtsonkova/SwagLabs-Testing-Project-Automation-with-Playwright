e0a0b23dd65acd8d8f4e31f880b234ff506f3475
mtsonkova <mtsonkova950@gmail.com>
2024-10-19 23:45:26 +0300

    Added ProductsPage.js file

---
+++ pageobjects/ProductsPage.js
@@ -0,0 +1,68 @@
+class ProductsPage{
+    constructor(page) {
+        this.page = page;
+        this.filterByNameAZ = page.getByText('Name (A to Z)');
+        this.filterByNameZA = page.getByText('Name (Z to A)');
+        this.filterByPriceLowHigh = page.getByText('Price (low to high)');
+        this.filterByPriceHighLow = page.getByText('Price (high to low)');
+
+        this.productsInfo = page.locator('.inventory_item_description');
+    }
+
+    async selectFilterByNameAtoZ() {
+        await this.filterByNameAZ.click();
+    }
+
+    async selectFilterByNameZtoA() {
+        await this.filterByNameZA.click();
+    }
+
+    async selectFilterByPriceLowHigh() {
+        await this.filterByPriceLowHigh.click();
+    }
+
+    async selectFilterByPriceHighLow() {
+        await this.filterByPriceHighLow.click();
+    }
+
+    async getFirstProductOnProductsPage() {
+        return await this.productsInfo.nth(0);
+    }
+
+    async getProductsWithPricesBelowOrEqualNum(price) {
+        let size = this.productsInfo.size();
+        let productsArr = [];
+       
+        for(let i = 0; i <= size; i++) {
+            let currentProduct = this.productsInfo[i];
+
+            let priceAsText = await currentProduct.locator('.inventory_item_price').textContent().slice(1);
+            let price = Number(priceAsText);
+
+            if(price < number) {
+                productsArr.push(currentProduct);
+            }
+        }
+        return productsArr;
+        
+    }
+
+    async getProductsWithPricesAboveOrEqualNum(price) {
+        let size = this.productsInfo.size();
+        let productsArr = [];
+       
+        for(let i = 0; i < size; i++) {
+            let currentProduct = this.productsInfo[i];
+
+            let priceAsText = await currentProduct.locator('.inventory_item_price').textContent().slice(1);
+            let price = Number(priceAsText);
+
+            if(price >= number) {
+                productsArr.push(currentProduct);
+            }
+        }
+        return productsArr;        
+    }
+}
+
+module.exports = {ProductsPage};
