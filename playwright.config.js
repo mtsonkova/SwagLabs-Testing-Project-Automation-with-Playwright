0d707937382a09bfd6d78daf7b0d8150f9029f6e
mtsonkova <mtsonkova950@gmail.com>
2025-01-19 19:38:15 +0200

    Changes in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html" { open: 'never' }], ["dot"], ["allure-playwright"]],
+  reporter: [["html", { open: 'never' }], ["dot"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,7 +30,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot: 'only-on-failure', // Take screenshots only on test failure
     video: 'retain-on-failure',
-    
+
     trace: 'on-first-retry',
   },
 
