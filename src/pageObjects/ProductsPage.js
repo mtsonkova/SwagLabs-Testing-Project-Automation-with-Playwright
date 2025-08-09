be7498acab4a810a8395099019e3beb7dafca058
mtsonkova <mtsonkova950@gmail.com>
2025-08-09 18:54:29 +0300

    Added error message in case of illegal parameter for getProducterIndex

--- src/pageObjects/ProductsPage.js
+++ src/pageObjects/ProductsPage.js
@@ -46,6 +46,9 @@ async selectFilter(filterValue) {
 }
 
     async getProductOnProductsPagePerIndex(index) {
+        if(index < 0 || index > his.productsPageLocators.productsInfo.length - 1) {
+            throw('Index cannot be smaller than 0 and greater than 5')
+        }
         return await this.productsPageLocators.productsInfo.nth(index);
     }
 }
